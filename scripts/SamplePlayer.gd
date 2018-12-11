e8e514a0720d72d54dd0c22666ca9f6fa75abcc2
Ronan Lina <connivinggod7@gmail.com>
2018-12-11 17:40:09 +0800

    set constants

--- scripts/SamplePlayer.gd
+++ scripts/SamplePlayer.gd
@@ -2,24 +2,28 @@ extends KinematicBody2D
 
 #floor normal (up direction) default value
 const UP = Vector2(0,-1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -500
+
 
 var motion = Vector2()
 
 func _physics_process(delta):
 	
-	motion.y += 10
+	motion.y += GRAVITY
 	
 	#check project settings for keys associated with the ff actions
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -SPEED
 	else:
 		motion.x = 0
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -400
+			motion.y = JUMP_HEIGHT
 	
 	#resets motion when collides
 	motion = move_and_slide(motion,UP)
