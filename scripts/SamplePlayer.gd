63714aad204c57e89a89bc3e87924c2c362b6873
Ronan Lina <connivinggod7@gmail.com>
2018-12-11 16:52:37 +0800

    left and right (x-axis) movement

---
+++ scripts/SamplePlayer.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = 100
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -100
+	else:
+		motion.x = 0
+		
+	move_and_slide(motion)
